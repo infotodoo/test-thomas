9ad728b56197c8a11f7dd674c851dedef8c0a1d4
Oscar B <ob@todoo.co>
2020-05-28 23:38:38 -0500

    calidad [FIX] access_thomas: test not sh entry ._.

--- access_restriction_by_ip/controllers/main.py
+++ access_restriction_by_ip/controllers/main.py
@@ -51,39 +51,12 @@ class Home(main.Home):
             ip_list.append(ip.ip_address)
 
         if not ip_address in ip_list:
-            values['error'] = _("Not allowed to login from this IP")
+            #values['error'] = _("Not allowed to login from this IP")
             return 'IP DO NOT ALLOWED'#request.render('web.login', values)
         else:
             if request.httprequest.method == 'POST':
                 old_uid = request.uid
-                # ip_address = request.httprequest.environ['REMOTE_ADDR']
-
                 if request.params['login']:
-                    # user_rec = request.env['res.users'].sudo().search(
-                    #     [('login', '=', request.params['login'])])
-                    # if user_rec.allowed_ips:
-                    #     ip_list = []
-                    #     for rec in user_rec.allowed_ips:
-                    #         ip_list.append(rec.ip_address)
-                    #     if ip_address in ip_list:
-                    #         try:
-                    #             uid = request.session.authenticate(
-                    #                 request.session.db,
-                    #                 request.params[
-                    #                     'login'],
-                    #                 request.params[
-                    #                     'password'])
-                    #             request.params['login_success'] = True
-                    #             return http.redirect_with_hash(
-                    #                 self._login_redirect(uid, redirect=redirect))
-                    #         except odoo.exceptions.AccessDenied as e:
-                    #             request.uid = old_uid
-                    #             if e.args == odoo.exceptions.AccessDenied().args:
-                    #                 values['error'] = _("Wrong login/password")
-                    #     else:
-                    #         request.uid = old_uid
-                    #         values['error'] = _("Not allowed to login from this IP")
-                    # else:
                     try:
                         uid = request.session.authenticate(request.session.db,
                                                         request.params[
@@ -99,3 +72,17 @@ class Home(main.Home):
                             values['error'] = _("Wrong login/password")
 
             return request.render('web.login', values)
+
+    @http.route()
+    def index(self, *args, **kw):
+        if request.session.uid and not request.env['res.users'].sudo().browse(request.session.uid).has_group('base.group_user'):
+            ip_address = request.httprequest.environ['REMOTE_ADDR']
+            ip_list = []
+            for ip in request.env['allowed.ips'].sudo().search([]):
+                ip_list.append(ip.ip_address)
+
+            if not ip_address in ip_list:
+                return 'IP DO NOT ALLOWED'
+
+            return http.local_redirect('/my', query=request.params, keep_hash=True)
+        return super(Home, self).index(*args, **kw)
